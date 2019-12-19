56829f46754b71b696e968fe57c4c48916721d3f
kipsikora <kipsikora@gmail.com>
2019-12-18 18:10:21 -0600

    Checkpoint3 Changes

--- src/Stress Reliever/Stress.js
+++ src/Stress Reliever/Stress.js
@@ -1,14 +1,26 @@
-let redValue = 0;
-let greenValue = 100;
+let redValue = 50;
+let blueValue = 100;
+let purpleValue = 1;
+// function changeColor() {
+//   $("#circle").click(function() {
+//     $(this).css({
+//       "background-image": `radial-gradient(circle, red ${redValue}, blue, green ${greenValue}%)`
+//     });
+//   });
+
 function changeColor() {
-  $("#circle").click(function() {
+  $("#circle2").click(function() {
     $(this).css({
-      "background-image": `radial-gradient(circle, red ${redValue}, blue, green ${greenValue}%)`
+      "background-image": `radial-gradient(circle, red ${redValue}%, blue ${blueValue}%, purple ${purpleValue}%)`
     });
+    $("audio#sound")[0].play();
   });
-  redValue += 5;
-  greenValue -= 2;
-  console.log(`red=(${redValue}) green=(${greenValue}`);
+
+  redValue -= 0.5;
+  blueValue -= 0.5;
+  purpleValue += 0.5;
+  // console.log(`red=(${redValue}) green=(${greenValue})`);
+  console.log(` red=(${redValue}) blue=(${blueValue}) purple=(${purpleValue})`);
 }
 // $("#circle").click(function(){
 //   $(this).css("background-image", "radial-gradient(circle, red 0, blue, green 100%);")
